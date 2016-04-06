d1f9deb808ad24baa63bef197d1d3d73fb63d983
heidilaursen <heidimlaursen@gmail.com>
2016-04-05 19:37:07 -0700

    added gulpfile.js

---
+++ HeidiLaursen/gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var eslint = require('gult-eslint');
+
+//add file here?
+
+//write lint task
+gulp.task('default', function() {
+  // place code for your default task here
+});
+
+//call lint task
